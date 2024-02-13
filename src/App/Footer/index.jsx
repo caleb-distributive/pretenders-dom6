3546ac7cbda620a96705a508d04d11443dfa884a
Caleb Aikens <zollqir@gmail.com>
2024-02-13 18:27:52 -0500

    add attribution

--- src/App/Footer/index.jsx
+++ src/App/Footer/index.jsx
@@ -15,10 +15,14 @@ function Footer(props) {
 	    <span className={styles.value}>{version}</span>
 	  </p>
 
-          <p className={styles.row}>
+    <p className={styles.row}>
 	    <span className={styles.caption}>Source code: </span>
 	    <a href="https://github.com/caleb-distributive/pretenders-dom6" className={styles.value}>https://github.com/caleb-distributive/pretenders-dom6</a>
 	  </p>
+    <p className={styles.row}>
+      <span className={styles.caption}>Based on the Dominions 5 Pretender Calculator by: </span>
+      <a href="https://github.com/bsielski" className={styles.value}>bsielski</a>
+    </p>
 
           <p className={styles.row}>
 	    Pretender cost seems wrong? Please create <a href="https://github.com/caleb-distributive/pretenders-dom6" className={styles.value}>a new issue</a> about it or mail me at <i>zollqir@gmail.com</i>.
