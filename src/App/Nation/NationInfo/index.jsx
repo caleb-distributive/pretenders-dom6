0f2c036096999c48f4f9660a0b2db4f252b39bd0
Caleb Aikens <zollqir@gmail.com>
2024-02-12 22:26:36 -0500

    update Nation element for Dominions 6

--- src/App/Nation/NationInfo/index.jsx
+++ src/App/Nation/NationInfo/index.jsx
@@ -3,37 +3,40 @@ import React from 'react';
 import styles from './NationInfo.module.scss';
 
 function NationInfo(props) {
-    const { f, a, w, e, s, d, n, b } = props.blessBonus;
-    const { heat, growth } = props.defaultScales;
+    const blessBonus = props.blessBonus;
+    const { order, productivity, heat, growth, fortune, magic} = props.defaultScales;
     
-    const fBonus = f ? " Fire +"   + f : "";
-    const aBonus = a ? " Air +"    + a : "";
-    const wBonus = w ? " Water +"  + w : "";
-    const eBonus = e ? " Earth +"  + e : "";
-    const sBonus = s ? " Astral +" + s : "";
-    const dBonus = d ? " Death +"  + d : "";
-    const nBonus = n ? " Nature +" + n : "";
-    const bBonus = b ? " Blood +"  + b : "";
-    const heatLabel = heat ? "Favourite temperature: "  + heat : "";
-    const growthLabel = growth ? "Default growth: "  + growth : "";
+    const blessBonusLabel   = blessBonus    ? `+${blessBonus}`                          : "";
+
+    const orderLabel        = order         ? `Default order: ${        order}`         : "";
+    const productivityLabel = productivity  ? `Default productivity: ${ productivity}`  : "";
+    const heatLabel         = heat          ? `Default temperature: ${  heat}`          : "";
+    const growthLabel       = growth        ? `Default growth: ${       growth}`        : "";
+    const fortuneLabel      = fortune       ? `Default fortune: ${      fortune}`       : "";
+    const magicLabel        = magic         ? `Default magic: ${        magic}`         : "";
     return (
 	<div className={styles.container}>
           <p>
-	    {(fBonus || aBonus || wBonus || eBonus || sBonus || dBonus || nBonus || bBonus) ? "Nation bless bonuses:" : ""}
-	    <span className={styles.blesses_fire}>{fBonus}</span>
-	    <span className={styles.blesses_air}>{aBonus}</span>
-	    <span className={styles.blesses_earth}>{eBonus}</span>
-	    <span className={styles.blesses_water}>{wBonus}</span>
-	    <span className={styles.blesses_astral}>{sBonus}</span>
-	    <span className={styles.blesses_death}>{dBonus}</span>
-	    <span className={styles.blesses_nature}>{nBonus}</span>
-	    <span className={styles.blesses_blood}>{bBonus}</span>
+	    {blessBonusLabel ? "Nation bless bonuses:" : ""}
+	    <span className={styles.blesses}>{blessBonusLabel}</span>
+          </p>
+          <p className={styles.order}>
+	    {orderLabel}
+          </p>
+          <p className={styles.productivity}>
+	    {productivityLabel}
           </p>
           <p className={styles.heat}>
 	    {heatLabel}
           </p>
           <p className={styles.growth}>
 	    {growthLabel}
+          </p>
+          <p className={styles.fortune}>
+	    {fortuneLabel}
+          </p>
+          <p className={styles.magic}>
+	    {magicLabel}
           </p>
 	</div>
     );
